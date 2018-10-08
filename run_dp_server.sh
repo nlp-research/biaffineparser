836bb9cb38fe3dc09f03e3a7237b340376fb1dee
Jongseong Woo <22februa@gmail.com>
2018-10-08 11:12:19 +0900

    modified to use CPU for REST API

--- run_dp_server.sh
+++ run_dp_server.sh
@@ -2,6 +2,9 @@
 
 HOST=0.0.0.0
 PORT=9001
-out_dir=parser/model_iss72-2
+out_dir=parser/model_iss73-5
+
+export CUDA_VISIBLE_DEVICES=-1
+echo CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 
 nohup python -um web.dp_server --host=$HOST --port=$PORT --out_dir=$out_dir &
